55c7f3bfde3b2fddeedeae4528ec5a57d737e11f
oceanesch <90210070+oceanesch@users.noreply.github.com>
2021-11-09 10:32:05 -0500

    Install libraries (#1)
    * adding MUI libraries
    
    * installing emotion
    
    * installing react router
    
    * trying out the router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from '../src/pages/LandingPage';
+import MyShelf from '../src/pages/MyShelf';
+
 function App() {
     return (
         <div className="App">
-            <h1>Welcome page My Library</h1>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/myshelf" element={<MyShelf />} />
+            </Routes>
         </div>
     );
 }
