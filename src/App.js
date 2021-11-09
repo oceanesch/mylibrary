18302fa03734248f425872b5f39d8f4ee1d8183d
oceanesch <90210070+oceanesch@users.noreply.github.com>
2021-11-09 10:46:46 -0500

    adding the diffrent pages (#2)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import LandingPage from '../src/pages/LandingPage';
 import MyShelf from '../src/pages/MyShelf';
+import AddNewBook from './pages/AddNewBook';
+import SignUp from './pages/SignUp';
 
 function App() {
     return (
@@ -8,6 +10,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/myshelf" element={<MyShelf />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/addnewbook" element={<AddNewBook />} />
             </Routes>
         </div>
     );
---
+++ src/pages/AddNewBook.js
@@ -0,0 +1,9 @@
+const AddNewBook = () => {
+    return (
+        <div>
+            <h1>ADD A NEW BOOK PAGE</h1>
+        </div>
+    );
+};
+
+export default AddNewBook;
