bcb56d6876bdb841e4031467a8d26a80b6d534e8
oceanesch <oceane.sch@gmail.com>
2021-11-09 09:57:13 -0500

    installing emotion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Button from '@mui/material/Button'
+
 function App() {
     return (
         <div className="App">
             <h1>Welcome page My Library</h1>
+            <Button variant="contained">Hello!</Button> 
         </div>
     );
 }
