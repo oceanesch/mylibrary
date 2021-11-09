7f3544d7f2be6d929d28459f6918e7e4a0e8fb98
oceanesch <oceane.sch@gmail.com>
2021-11-09 11:42:11 -0500

    creating a color theme and providing it to the appbar

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,14 +1,18 @@
 import AppBar from '@mui/material/AppBar';
 import ToolBar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
+import theme from '../theme';
+import { ThemeProvider } from '@mui/material/styles';
 
 const MainNavigation = () => {
     return (
-        <AppBar position="static">
-            <ToolBar>
-                <Button color="inherit">Login</Button>
-            </ToolBar>
-        </AppBar>
+        <ThemeProvider theme={theme}>
+            <AppBar position="static">
+                <ToolBar>
+                    <Button color="inherit">Login</Button>
+                </ToolBar>
+            </AppBar>
+        </ThemeProvider>
     );
 };
 
---
+++ src/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#fc8caa',
+            light: '#ffbedc',
+            dark: '#c75c7b',
+        },
+        secondary: {
+            main: '#ffecb9',
+            light: '#ffffec',
+            dark: '#cbba89',
+        },
+    },
+});
+
+export default theme;
