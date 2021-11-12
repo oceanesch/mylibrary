9e9bae746833369f83c9ac5db05d6fbf5e22d2a4
oceanesch <oceane.sch@gmail.com>
2021-11-12 16:07:22 -0500

    working on the front of the landing page: adding 2 cards, colors, typography and a button to sign up

--- src/theme.js
+++ src/theme.js
@@ -2,12 +2,12 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
     palette: {
-        primary: {
+        secondary: {
             main: '#fc8caa',
             light: '#ffbedc',
             dark: '#c75c7b',
         },
-        secondary: {
+        primary: {
             main: '#ffecb9',
             light: '#ffffec',
             dark: '#cbba89',
