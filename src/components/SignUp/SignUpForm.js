c490e3cd2eab85d85fe9ebe83f5c1f82a27eab2f
oceanesch <oceane.sch@gmail.com>
2021-11-15 14:43:24 -0500

    adding basic styling to the form

--- src/components/SignUp/SignUpForm.js
+++ src/components/SignUp/SignUpForm.js
@@ -1,15 +1,16 @@
+import { useState } from 'react';
 import {
     FormControl,
     IconButton,
     InputAdornment,
     InputLabel,
     OutlinedInput,
+    Button,
+    Box,
+    TextField,
+    Grid,
 } from '@mui/material';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Visibility from '@mui/icons-material/Visibility';
-import { useState } from 'react';
-import { VisibilityOff } from '@mui/icons-material';
+import { VisibilityOff, Visibility } from '@mui/icons-material';
 
 const SignUpForm = () => {
     const [passwordInput, setPasswordInput] = useState('');
@@ -28,103 +29,81 @@ const SignUpForm = () => {
     };
 
     return (
-        <Box>
-            <TextField
-                id="first-name-input"
-                variant="outlined"
-                label="First Name"
-                required
-            />
-            <TextField
-                id="last-name-input"
-                variant="outlined"
-                label="Last Name"
-                required
-            />
-            <TextField
-                id="email-address-input"
-                variant="outlined"
-                label="E-Mail Address"
-                required
-                type="email"
-            />
-            <FormControl variant="outlined">
-                <InputLabel htmlFor="password-input">Password</InputLabel>
-                <OutlinedInput
-                    id="password-input"
-                    type={showPassword ? 'text' : 'password'}
-                    value={passwordInput}
-                    onChange={changeHandler}
-                    label="password"
-                    required
-                    endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton
-                                aria-label="toggle password visibility"
-                                edge="end"
-                                onClick={clickShowPasswordHandler}
-                                onMouseDown={mouseDownPasswordHandler}
-                            >
-                                {showPassword ? (
-                                    <VisibilityOff />
-                                ) : (
-                                    <Visibility />
-                                )}
-                            </IconButton>
-                        </InputAdornment>
-                    }
-                ></OutlinedInput>
-            </FormControl>
+        <Box sx={{ xs:4 }}>
+            <Grid
+                container
+                direction="column"
+                alignItems="center"
+                justifyContent="space-around"
+            >
+                <Grid item>
+                    <TextField
+                        id="first-name-input"
+                        variant="outlined"
+                        label="First Name"
+                        required
+                        margin="normal"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item>
+                    <TextField
+                        id="last-name-input"
+                        variant="outlined"
+                        label="Last Name"
+                        required
+                        margin="normal"
+                    />
+                </Grid>
+                <Grid item>
+                    <TextField
+                        id="email-address-input"
+                        variant="outlined"
+                        label="E-Mail Address"
+                        required
+                        type="email"
+                        margin="normal"
+                    />
+                </Grid>
+                <Grid item>
+                    <FormControl variant="outlined" margin="normal">
+                        <InputLabel htmlFor="password-input">
+                            Password
+                        </InputLabel>
+                        <OutlinedInput
+                            id="password-input"
+                            type={showPassword ? 'text' : 'password'}
+                            value={passwordInput}
+                            onChange={changeHandler}
+                            label="password"
+                            required
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        edge="end"
+                                        onClick={clickShowPasswordHandler}
+                                        onMouseDown={mouseDownPasswordHandler}
+                                    >
+                                        {showPassword ? (
+                                            <VisibilityOff />
+                                        ) : (
+                                            <Visibility />
+                                        )}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        ></OutlinedInput>
+                    </FormControl>
+                </Grid>
+                <Grid item>
+                    <Button variant="outlined" margin="normal">
+                        Sign Up
+                    </Button>
+                </Grid>
+            </Grid>
         </Box>
     );
 };
-
-/*
-const [values, setValues] = React.useState({
-    amount: '',
-    password: '',
-    weight: '',
-    weightRange: '',
-    showPassword: false,
-  });
-
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
-
-  const handleClickShowPassword = () => {
-    setValues({
-      ...values,
-      showPassword: !values.showPassword,
-    });
-  };
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
-
-<FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={values.showPassword ? 'text' : 'password'}
-            value={values.password}
-            onChange={handleChange('password')}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          />
-        </FormControl>
-*/
 
 export default SignUpForm;
