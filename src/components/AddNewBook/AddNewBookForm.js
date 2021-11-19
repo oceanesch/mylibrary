6e651e2f99755fc4ea06ac301272619476a87a06
oceanesch <oceane.sch@gmail.com>
2021-11-19 14:55:02 -0500

    retrieving inputs

--- src/components/AddNewBook/AddNewBookForm.js
+++ src/components/AddNewBook/AddNewBookForm.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import TextField from '@mui/material/TextField';
 import { Button, Grid } from '@mui/material';
 
 const AddNewBookForm = () => {
+    const titleInputRef = useRef();
+    const authorInputRef = useRef();
+    const coverInputRef = useRef();
+
+    const addNewBookSubmitHandler = (event) => {
+        event.preventDefault();
+        const titleInput = titleInputRef.current.value;
+        const authorInput = authorInputRef.current.value;
+        const coverInput = coverInputRef.current.value;
+
+        console.log(titleInput, authorInput, coverInput);
+
+        titleInputRef.current.value = '';
+        authorInputRef.current.value = '';
+        coverInputRef.current.value = '';
+    };
+
     return (
         <React.Fragment>
-            <form>
+            <form onSubmit={addNewBookSubmitHandler}>
                 <Grid
                     container
                     direction="column"
@@ -19,6 +36,7 @@ const AddNewBookForm = () => {
                             label="Title"
                             required
                             margin="normal"
+                            inputRef={titleInputRef}
                         />
                     </Grid>
                     <Grid item>
@@ -28,6 +46,7 @@ const AddNewBookForm = () => {
                             label="Author"
                             required
                             margin="normal"
+                            inputRef={authorInputRef}
                         />
                     </Grid>
                     <Grid item>
@@ -37,10 +56,13 @@ const AddNewBookForm = () => {
                             label="URL of the cover"
                             required
                             margin="normal"
+                            inputRef={coverInputRef}
                         />
                     </Grid>
                     <Grid item>
-                        <Button variant="outlined">Submit</Button>
+                        <Button variant="outlined" type="submit">
+                            Submit
+                        </Button>
                     </Grid>
                 </Grid>
             </form>
