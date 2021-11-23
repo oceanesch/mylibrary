5b460944687983791866e54febdf589df0919825
oceanesch <oceane.sch@gmail.com>
2021-11-23 15:38:39 -0500

    retrieve input from the login

--- src/components/LogIn/LogInForm.js
+++ src/components/LogIn/LogInForm.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import {
     Box,
     TextField,
@@ -8,7 +8,7 @@ import {
     OutlinedInput,
     IconButton,
     InputAdornment,
-    Button
+    Button,
 } from '@mui/material';
 
 import { VisibilityOff, Visibility } from '@mui/icons-material';
@@ -29,62 +29,87 @@ const LogInForm = () => {
         event.preventDefault();
     };
 
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
+
+    const LogInSubmitHandler = (event) => {
+        event.preventDefault();
+
+        const emailInput = emailInputRef.current.value;
+        const passwordInput = passwordInputRef.current.value;
+
+        console.log(emailInput, passwordInput)
+
+        emailInputRef.current.value=""
+        passwordInputRef.current.value=""
+    };
+
     return (
-        <Box sx={{ xs: 4 }}>
-            <Grid
-                container
-                direction="column"
-                alignItems="center"
-                justifyContent="space-around"
-            >
-                <Grid item>
-                    <TextField
-                        id="email-address-input"
-                        variant="outlined"
-                        label="E-Mail Address"
-                        required
-                        type="email"
-                        margin="normal"
-                    />
-                </Grid>
-                <Grid item>
-                    <FormControl variant="outlined" margin="normal">
-                        <InputLabel htmlFor="password-input">
-                            Password
-                        </InputLabel>
-                        <OutlinedInput
-                            id="password-input"
-                            type={showPassword ? 'text' : 'password'}
-                            value={passwordInput}
-                            onChange={changeHandler}
-                            label="password"
+        <form onSubmit={LogInSubmitHandler}>
+            <Box sx={{ xs: 4 }}>
+                <Grid
+                    container
+                    direction="column"
+                    alignItems="center"
+                    justifyContent="space-around"
+                >
+                    <Grid item>
+                        <TextField
+                            id="email-address-input"
+                            variant="outlined"
+                            label="E-Mail Address"
                             required
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        aria-label="toggle password visibility"
-                                        edge="end"
-                                        onClick={clickShowPasswordHandler}
-                                        onMouseDown={mouseDownPasswordHandler}
-                                    >
-                                        {showPassword ? (
-                                            <VisibilityOff />
-                                        ) : (
-                                            <Visibility />
-                                        )}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                        ></OutlinedInput>
-                    </FormControl>
+                            type="email"
+                            margin="normal"
+                            inputRef={emailInputRef}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <FormControl variant="outlined" margin="normal">
+                            <InputLabel htmlFor="password-input">
+                                Password
+                            </InputLabel>
+                            <OutlinedInput
+                                id="password-input"
+                                type={showPassword ? 'text' : 'password'}
+                                value={passwordInput}
+                                onChange={changeHandler}
+                                label="password"
+                                required
+                                inputRef={passwordInputRef}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            edge="end"
+                                            onClick={clickShowPasswordHandler}
+                                            onMouseDown={
+                                                mouseDownPasswordHandler
+                                            }
+                                        >
+                                            {showPassword ? (
+                                                <VisibilityOff />
+                                            ) : (
+                                                <Visibility />
+                                            )}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                            ></OutlinedInput>
+                        </FormControl>
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            variant="outlined"
+                            margin="normal"
+                            type="submit"
+                        >
+                            Sign Up
+                        </Button>
+                    </Grid>
                 </Grid>
-                <Grid item>
-                    <Button variant="outlined" margin="normal">
-                        Sign Up
-                    </Button>
-                </Grid>
-            </Grid>
-        </Box>
+            </Box>
+        </form>
     );
 };
 
