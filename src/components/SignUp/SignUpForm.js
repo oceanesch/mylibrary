f49680590534448e398e70858f4c1b7b886d8246
oceanesch <oceane.sch@gmail.com>
2021-11-25 14:14:52 -0500

    sending a post request to the auth API when signin up

--- src/components/SignUp/SignUpForm.js
+++ src/components/SignUp/SignUpForm.js
@@ -2,59 +2,84 @@ import { useRef } from 'react';
 import { Button, Box, Grid } from '@mui/material';
 
 import PasswordInput from '../Shared/PasswordInput';
-import FirstNameInput from '../Shared/FirstNameInput';
-import LastNameInput from '../Shared/LastNameInput';
+// import FirstNameInput from '../Shared/FirstNameInput';
+// import LastNameInput from '../Shared/LastNameInput';
 import EmailInput from '../Shared/EmailInput';
 
 const SignUpForm = () => {
-    const firstNameInputRef = useRef();
-    const lastNameInputRef = useRef();
+    // const firstNameInputRef = useRef();
+    // const lastNameInputRef = useRef();
     const emailInputRef = useRef();
     const passwordInputRef = useRef();
 
     const signUpSubmitHandler = (event) => {
         event.preventDefault();
 
-        const firstNameInput = firstNameInputRef.current.value;
-        const lastNameInput = lastNameInputRef.current.value;
-        const emailInput = emailInputRef.current.value;
-        const passwordInput = passwordInputRef.current.value;
+        // const enteredFirstName = firstNameInputRef.current.value;
+        // const enteredLastName = lastNameInputRef.current.value;
+        const enteredEmail = emailInputRef.current.value;
+        const enteredPassword = passwordInputRef.current.value;
 
-        console.log(firstNameInput, lastNameInput, emailInput, passwordInput);
+        // add validation after here
+
+        fetch(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDe2VlLrzxhf8f_PW46fjEuMfYRy6yDvSY',
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    // firstName: enteredFirstName,
+                    // lastName: enteredLastName,
+                    email: enteredEmail,
+                    password: enteredPassword,
+                    returnSecureToken: true,
+                }),
+                headers: { 'Content-Type': 'application/json' },
+            }
+        )
+        // .then((responsePayLoad) => {
+        //     if(responsePayLoad.ok) {
+        //         //do something w/ the success response 
+        //     } else{
+        //         //do something if you get an error
+        //     }
+        // })
+
+        console.log(
+            // enteredFirstName,
+            // enteredLastName,
+            enteredEmail,
+            enteredPassword
+        );
     };
 
     return (
         <Box sx={{ xs: 4 }}>
-                <Grid
-                    container
-                    direction="column"
-                    alignItems="center"
-                    justifyContent="space-around"
-                    component="form"
-                    onSubmit={signUpSubmitHandler}
-                >
-                    <Grid item>
-                        <FirstNameInput firstNameInputRef={firstNameInputRef} />
-                    </Grid>
-                    <Grid item>
-                        <LastNameInput lastNameInputRef={lastNameInputRef} />
-                    </Grid>
-                    <Grid item>
-                        <EmailInput emailInputRef={emailInputRef} />
-                    </Grid>
-                    <Grid item>
-                        <PasswordInput passwordInputRef={passwordInputRef} />
-                    </Grid>
-                    <Grid item>
-                        <Button
-                            variant="outlined"
-                            margin="normal"
-                            type="submit"
-                        >
-                            Sign Up
-                        </Button>
-                    </Grid>
+            <Grid
+                container
+                direction="column"
+                alignItems="center"
+                justifyContent="space-around"
+                component="form"
+                onSubmit={signUpSubmitHandler}
+            >
+                {/* <Grid item>
+                    <FirstNameInput firstNameInputRef={firstNameInputRef} />
                 </Grid>
+                <Grid item>
+                    <LastNameInput lastNameInputRef={lastNameInputRef} />
+                </Grid> */}
+                <Grid item>
+                    <EmailInput emailInputRef={emailInputRef} />
+                </Grid>
+                <Grid item>
+                    <PasswordInput passwordInputRef={passwordInputRef} />
+                </Grid>
+                <Grid item>
+                    <Button variant="outlined" margin="normal" type="submit">
+                        Sign Up
+                    </Button>
+                </Grid>
+            </Grid>
         </Box>
     );
 };
