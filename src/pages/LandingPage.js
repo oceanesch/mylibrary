e475b2081d3c3328299b6b31860091c34b6d9274
oceanesch <90210070+oceanesch@users.noreply.github.com>
2021-12-14 16:49:17 -0500

    design & responsive design landing page (#17)
    * wip
    
    * wip
    
    * working on the responsive design of big screens

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,57 +1,74 @@
-import Button from '@mui/material/Button';
 import React from 'react';
-import MainNavigation from '../components/Layout/MainNavigation';
-import Card from '../components/UI/Card';
-import Grid from '@mui/material/Grid';
-import classes from './LandingPage.module.css';
-import Stack from '@mui/material/Stack';
 import { Link as RouterLink } from 'react-router-dom';
+
+import styles from './LandingPage.module.css';
+
+import { StyledEngineProvider } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+import MainNavigation from '../components/Layout/MainNavigation';
 
 const LandingPage = () => {
     return (
         <React.Fragment>
-            <MainNavigation />
-            <h1 className={classes.mainTitle}>Welcome to MyLibrary</h1>
-            <Grid container alignItems="center" justifyContent="space-around">
-                <Grid item xs={5}>
-                    <Card>
-                        <p className={classes.p}>
+            <StyledEngineProvider injectFirst>
+                <MainNavigation />
+                <Typography component="h1" className={styles.mainTitle}>
+                    Welcome to MyLibrary
+                </Typography>
+                <Grid
+                    container
+                    direction="column"
+                    className={styles.mainBody}
+                    // alignItems="center"
+                    // justifyContent="space-around"
+                >
+                    <Grid item xs={1} md={6} className={styles.gridItem}>
+                        <Box className={styles.card}>
                             MyLibrary is an app that helps you track your books.
                             You can add the books you read and from there, add
                             your favorite quotes or thoughts or even sort them
                             by tags. MyLibrary will gather reading stats that
                             will help you with your future reading.
-                        </p>
-
-                        <h2 className={classes.h2}>
-                            Start now by signing up !{' '}
-                        </h2>
-                    </Card>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={1} md={6} className={styles.gridItem}>
+                        <Box className={styles.card}>
+                            <Stack
+                                justifyContent="space-evenly"
+                                alignItems="center"
+                                // spacing={2}
+                                textAlign="center"
+                            >
+                                <Typography
+                                    component="h2"
+                                    className={styles.cardTitle}
+                                >
+                                    JOIN MYLIBRARY AND START TRACKING YOUR
+                                    READING NOW !
+                                </Typography>
+                                <img
+                                    src="https://media.istockphoto.com/photos/stack-of-books-on-living-coral-background-picture-id1151417884?b=1&k=20&m=1151417884&s=170667a&w=0&h=A2Roz8X3rEyoV45LeWbybx9i0TtRqdch2qlgJKZHGlU="
+                                    alt="stack of books"
+                                    height="400px"
+                                />
+                                <Button
+                                    variant="contained"
+                                    component={RouterLink}
+                                    to="/signup"
+                                    className={styles.cardButton}
+                                >
+                                    SIGN UP
+                                </Button>
+                            </Stack>
+                        </Box>
+                    </Grid>
                 </Grid>
-                <Grid item xs={5}>
-                    <Card>
-                        <Stack
-                            justifyContent="space-evenly"
-                            alignItems="center"
-                            spacing={2}
-                            textAlign="center"
-                        >
-                            <h1 className={classes.h1}>
-                                JOIN MYLIBRARY AND START TRACKING YOUR READING
-                                NOW
-                            </h1>
-                            <img
-                                src="https://media.istockphoto.com/photos/stack-of-books-on-living-coral-background-picture-id1151417884?b=1&k=20&m=1151417884&s=170667a&w=0&h=A2Roz8X3rEyoV45LeWbybx9i0TtRqdch2qlgJKZHGlU="
-                                alt="stack of books"
-                                height="300px"
-                            />
-                            <Button component={RouterLink} to="/signup">
-                                SIGN UP
-                            </Button>
-                        </Stack>
-                    </Card>
-                </Grid>
-            </Grid>
+            </StyledEngineProvider>
         </React.Fragment>
     );
 };
