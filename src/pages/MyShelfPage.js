6c4e206ec469cc3b408b99974292300568cf35d6
oceanesch <oceane.sch@gmail.com>
2021-12-16 10:19:26 -0500

    working on the front design of the title and main button of myShelf page

--- src/pages/MyShelfPage.js
+++ src/pages/MyShelfPage.js
@@ -1,18 +1,45 @@
 import React from 'react';
-import MainNavigation from '../components/Layout/MainNavigation';
+import { Link as RouterLink } from 'react-router-dom';
+import styles from './MyShelfPage.module.css';
+import { StyledEngineProvider } from '@mui/material/styles';
 import BookList from '../components/MyShelf/BooksList';
 import Button from '@mui/material/Button';
-import { Link as RouterLink } from 'react-router-dom';
+import MainNavigation from '../components/Layout/MainNavigation';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 
 const MyShelf = () => {
     return (
         <React.Fragment>
-            <MainNavigation />
-            <h1>MY SHELF PAGE</h1>
-            <Button component={RouterLink} to="/addnewbook" variant="contained">
-                ADD A NEW BOOK
-            </Button>
-            <BookList />
+            <StyledEngineProvider injectFirst>
+                <MainNavigation />
+                <Grid
+                    container
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <Grid item>
+                        <Typography component="h1" className={styles.mainTitle}>
+                            MY SHELF
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            component={RouterLink}
+                            to="/addnewbook"
+                            variant="contained"
+                            className={styles.button}
+                            size="large"
+                        >
+                            ADD A NEW BOOK
+                        </Button>
+                    </Grid>
+                    <Grid item>
+                        <BookList />
+                    </Grid>
+                </Grid>
+            </StyledEngineProvider>
         </React.Fragment>
     );
 };
