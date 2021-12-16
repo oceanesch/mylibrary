432d74bc9bc5993f00634280a86ba62cf133f16e
oceanesch <90210070+oceanesch@users.noreply.github.com>
2021-12-16 15:16:24 -0500

    Front my shelf page (#18)
    * working on the front design of the title and main button of myShelf page
    
    * working on buttons
    
    * adding css to the book card
    
    * working on the responsive of the shelf page

--- src/components/MyShelf/BookItem.js
+++ src/components/MyShelf/BookItem.js
@@ -1,54 +1,46 @@
-import {
-    Card,
-    CardContent,
-    CardMedia,
-    Typography,
-    IconButton,
-} from '@mui/material';
-import { Delete, Edit } from '@mui/icons-material';
 import React from 'react';
-import classes from './BookItem.module.css';
+import styles from './BookItem.module.css';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardActions from '@mui/material/CardActions';
+import IconButton from '@mui/material/IconButton';
+import { Delete, Edit } from '@mui/icons-material';
 
 const BookItem = (props) => {
     const deleteButtonHandler = (event) => {
         event.preventDefault();
-        const deletedBookID = props.id
-        props.onSaveDeletedBookId(deletedBookID)
+        const deletedBookID = props.id;
+        props.onSaveDeletedBookId(deletedBookID);
     };
 
     return (
         <React.Fragment>
-            <Card
-                variant="elevation"
-                elevation={3}
-                className={classes.bookCard}
-            >
+            <Card variant="elevation" elevation={3} className={styles.bookCard}>
                 <CardMedia
                     component="img"
                     image={props.image}
-                    sx={{ width: 170, height: 255 }}
                     alt={`Cover of ${props.title}`}
+                    className={styles.bookCardImage}
                 />
-                <CardContent className={classes.bookCardContent}>
-                    <Typography
-                        component="h5"
-                        variant="h5"
-                        className={classes.bookCardTitle}
-                    >
+                <CardContent className={styles.bookCardContent}>
+                    <Typography component="h5" className={styles.bookCardTitle}>
                         {props.title}
                     </Typography>
-                    <Typography component="h6" variant="overline">
-                        {props.author}
-                    </Typography>
+                    <Typography variant="overline">{props.author}</Typography>
                 </CardContent>
-                <CardContent>
+                <CardActions className={styles.bookCardActions}>
                     <IconButton aria-label="modify book">
                         <Edit />
                     </IconButton>
-                    <IconButton aria-label="delete book" onClick={deleteButtonHandler}>
+                    <IconButton
+                        aria-label="delete book"
+                        onClick={deleteButtonHandler}
+                    >
                         <Delete />
                     </IconButton>
-                </CardContent>
+                </CardActions>
             </Card>
         </React.Fragment>
     );
