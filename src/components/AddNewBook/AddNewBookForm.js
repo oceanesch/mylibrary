8b9ccf03adb3b1b1a348a3e2b6401d0ace0eae11
oceanesch <oceane.sch@gmail.com>
2021-12-17 14:30:24 -0500

    working on the front and responsive design of the add new book page

--- src/components/AddNewBook/AddNewBookForm.js
+++ src/components/AddNewBook/AddNewBookForm.js
@@ -1,8 +1,11 @@
 import React, { useRef } from 'react';
-import TextField from '@mui/material/TextField';
-import { Button, Grid } from '@mui/material';
-import { addNewBook } from '../../libs/api';
 import { useNavigate } from 'react-router-dom';
+import styles from './AddNewBookForm.module.css';
+import { StyledEngineProvider } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import { addNewBook } from '../../libs/api';
 
 const AddNewBookForm = () => {
     const titleInputRef = useRef();
@@ -30,50 +33,38 @@ const AddNewBookForm = () => {
 
     return (
         <React.Fragment>
-            <form onSubmit={addNewBookSubmitHandler}>
-                <Grid
-                    container
-                    direction="column"
-                    alignItems="center"
-                    justifyContent="space-around"
-                >
-                    <Grid item>
-                        <TextField
-                            id="title-input"
-                            variant="outlined"
-                            label="Title"
-                            required
-                            margin="normal"
-                            inputRef={titleInputRef}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <TextField
-                            id="author-input"
-                            variant="outlined"
-                            label="Author"
-                            required
-                            margin="normal"
-                            inputRef={authorInputRef}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <TextField
-                            id="image-input"
-                            variant="outlined"
-                            label="URL of the cover"
-                            required
-                            margin="normal"
-                            inputRef={coverInputRef}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <Button variant="outlined" type="submit">
-                            Submit
-                        </Button>
-                    </Grid>
-                </Grid>
-            </form>
+            <StyledEngineProvider>
+                <form onSubmit={addNewBookSubmitHandler}>
+                    <TextField
+                        id="title-input"
+                        variant="outlined"
+                        label="Title"
+                        required
+                        inputRef={titleInputRef}
+                    />
+                    <TextField
+                        id="author-input"
+                        variant="outlined"
+                        label="Author"
+                        required
+                        inputRef={authorInputRef}
+                    />
+                    <TextField
+                        id="image-input"
+                        variant="outlined"
+                        label="URL of the cover"
+                        required
+                        inputRef={coverInputRef}
+                    />
+                    <Button
+                        variant="outlined"
+                        type="submit"
+                        className={styles.button}
+                    >
+                        Submit
+                    </Button>
+                </form>
+            </StyledEngineProvider>
         </React.Fragment>
     );
 };
